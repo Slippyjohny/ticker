b32ee8bbfbe5eb4562191aa234ccefa5f8fc7b20
SlippyJohny <shpak.d.o@ukr.net>
2017-01-06 16:34:44 +0200

    tests for CRUD

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,16 @@
 class ProjectsController < ApplicationController
   def index
-
+    @projects = Project.all
   end
   def new
     @project = Project.new
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
     @project = Project.find(params[:id])
   end
 
@@ -17,11 +21,30 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      # nothing, yet
+      flash.now[:alert] = "Project has not been created."
+      render "new"
     end
   end
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render "edit"
+    end
+  end
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
+  private
   def project_params
     params.require(:project).permit(:name, :description)
   end
+
 
 end
